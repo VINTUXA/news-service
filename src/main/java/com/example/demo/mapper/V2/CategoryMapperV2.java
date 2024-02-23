75cc01a74588b6a50dc525f1de76389e15dbc4f8
VINTUXA <dvo.performance@gmail.com>
2024-02-24 00:08:35 +0300

    fix all mappers again

--- src/main/java/com/example/demo/mapper/V2/CategoryMapperV2.java
+++ src/main/java/com/example/demo/mapper/V2/CategoryMapperV2.java
@@ -7,6 +7,7 @@ import com.example.demo.web.model.CategoryListResponse;
 import com.example.demo.web.model.CategoryResponse;
 import com.example.demo.web.model.UpsetCategoryRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -15,10 +16,13 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = {CommentMapperV2.class, NewsMapperV2.class})
 public interface CategoryMapperV2 {
 
+    @Mapping(target = "name", source = "categoryName")
     Category requestToCategory(UpsetCategoryRequest request);
 
+    @Mapping(target = "name", source = "request.categoryName")
     public Category requestToCategory(Long categoryId, UpsetCategoryRequest request);
 
+    @Mapping(target = "categoryName", source = "name")
     public CategoryResponse categoryToResponse(Category category);
 
     default List<CategoryResponse> categoryListToResponseList(List<Category> categories){
